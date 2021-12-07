b06617364048579577e8be1046c2a14c3e0d3da6
Jacqueline <jdkerg@gmail.com>
2021-12-07 16:47:29 -0500

    genre tables, sql update, database queries

---
+++ dotnet/Capstone/DAO/AccountSqlDao.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Threading.Tasks;
+using Capstone.Models;
+
+namespace Capstone.DAO
+{
+    public class AccountSqlDao : IAccountDao
+    {
+        private readonly string connectionString;
+        public AccountSqlDao(string dbConnectionString)
+        {
+            connectionString = dbConnectionString;
+        }
+
+        public Account GetAccount(int accountId)
+        {
+            Account returnAccount = null;
+
+            try
+            {
+                using (SqlConnection conn = new SqlConnection(connectionString))
+                {
+                    conn.Open();
+
+                    SqlCommand cmd = new SqlCommand("SELECT accounts.accountId, accounts.userId, profileName " +
+                        "FROM accounts " +
+                        "JOIN genre_accounts ON account.account_id = genre.account_id " +
+                        "JOIN genres ON genre.genre_id = genre_accounts.genre_id" +
+                        "WHERE accounts.accountId = @accountId", conn);
+                    cmd.Parameters.AddWithValue("@accountId", accountId);
+                    SqlDataReader reader = cmd.ExecuteReader();
+
+                    if (reader.Read())
+                    {
+                        returnAccount = GetAccountFromReader(reader);
+                    }
+                }
+            }
+            catch (SqlException)
+            {
+                throw;
+            }
+
+            return returnAccount;
+        }
+
+        public Account AddAccount(int userId, List<string> favoriteGenres, string profileName)
+        {
+            int newAccountId;
+            try
+            {
+                using (SqlConnection conn = new SqlConnection(connectionString))
+                {
+                    conn.Open();
+
+                    SqlCommand cmd = new SqlCommand("INSERT INTO accounts (accounts.userId, favoriteGenres, profileName) OUTPUT INSERTED.accountId VALUES (@accounts.userId, @favoriteGenres, @profileName)", conn);
+                    cmd.Parameters.AddWithValue("@accounts.userId", userId);
+                    cmd.Parameters.AddWithValue("@favoriteGenres", favoriteGenres);
+                    cmd.Parameters.AddWithValue("@profileName", profileName);
+                    newAccountId = Convert.ToInt32(cmd.ExecuteScalar());
+                }
+            }
+            catch (SqlException)
+            {
+                throw;
+            }
+
+            return GetAccount(newAccountId);
+        }
+
+        private Account GetAccountFromReader(SqlDataReader reader)
+        {
+            Account accountFromReader = new Account()
+            {
+                AccountId = Convert.ToInt32(reader["account_id"]),
+                UserId = Convert.ToInt32(reader["user_id"]),
+                //FavoriteGenres = Convert.To(reader["favoriteGenres"]),
+                ProfileName = Convert.ToString(reader["profileName"]),
+            };
+
+            return accountFromReader;
+        }
+    }
+}
--- dotnet/database/capstone.sql
+++ dotnet/database/capstone.sql
@@ -33,6 +33,20 @@ CREATE TABLE accounts (
 	CONSTRAINT FK_accounts_users FOREIGN KEY (user_id) references users (user_id)
 )
 
+CREATE TABLE genres (
+	genre_id int IDENTITY(1, 1) NOT NULL,
+	genre_name varchar(50) NOT NULL
+	CONSTRAINT PK_genres PRIMARY KEY (genre_id)
+)
+
+CREATE TABLE genre_accounts (
+	genre_id int NOT NULL,
+	account_id int NOT NULL
+	CONSTRAINT PK_genre_accounts PRIMARY KEY (genre_id, account_id),
+	CONSTRAINT FK_genre_accounts_genres FOREIGN KEY (genre_id) references genres (genre_id),
+	CONSTRAINT FK_genre_accounts_accounts FOREIGN KEY (account_id) references accounts (account_id)
+)
+
 --populate default data
 INSERT INTO users (username, password_hash, salt, user_role) VALUES ('user','Jg45HuwT7PZkfuKTz6IB90CtWY4=','LHxP4Xh7bN0=','user');
 INSERT INTO users (username, password_hash, salt, user_role) VALUES ('admin','YhyGVQ+Ch69n4JMBncM4lNF/i9s=', 'Ar/aB2thQTI=','admin');
