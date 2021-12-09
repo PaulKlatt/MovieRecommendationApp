09c99a4c07824dacb6f8904113021160ba9087c2
Paul Klatt <bcingul8r@gmail.com>
2021-12-09 11:57:52 -0500

    Genre list on home page updated

--- vue/src/components/UserHomepage.vue
+++ vue/src/components/UserHomepage.vue
@@ -3,32 +3,91 @@
       <h2>Choose the genres you would like to browse</h2>
       <form class="genre-form">
         <ul class="genres">
+          <li >
+
           <li>
-            <input name="action" type="checkbox"/>
-            <label for="action">Action</label>
+            <input name="Action" type="checkbox"/>
+            <label for="Action">Action</label>
 
           </li>
           <li>
-            <input name="romcom" type="checkbox"/>
-            <label for="romcom">RomCom</label>
+            <input name="Adventure" type="checkbox"/>
+            <label for="Adventure">Adventure</label>
 
           </li>
           <li>
-            <input name="horror" type="checkbox"/>
-            <label for="horror">Horror</label>
+            <input name="Animation" type="checkbox"/>
+            <label for="Animation">Animation</label>
 
           </li>
           <li>
-            <input name="drama" type="checkbox"/>
-            <label for="drama">Drama</label>
+            <input name="Comedy" type="checkbox"/>
+            <label for="Comedy">Comedy</label>
 
           </li>
           <li>
-            <input name="comedy" type="checkbox"/>
-            <label for="comedy">Comedy</label>
-
+            <input name="Crime" type="checkbox"/>
+            <label for="Crime">Crime</label>
+          </li>
+          <li>
+            <input name="Documentary" type="checkbox"/>
+            <label for="Documentary">Documentary</label>
+          </li>
+          <li>
+            <input name="Drama" type="checkbox"/>
+            <label for="Drama">Drama</label>
+          </li>
+          <li>
+            <input name="Family" type="checkbox"/>
+            <label for="Family">Family</label>
+          </li>
+          <li>
+            <input name="Fantasy" type="checkbox"/>
+            <label for="Fantasy">Fantasy</label>
+          </li>
+          <li>
+            <input name="History" type="checkbox"/>
+            <label for="History">History</label>
+          </li>
+          <li>
+            <input name="Horror" type="checkbox"/>
+            <label for="Horror">Horror</label>
+          </li>
+          <li>
+            <input name="Music" type="checkbox"/>
+            <label for="Music">Music</label>
+          </li>
+          <li>
+            <input name="Mystery" type="checkbox"/>
+            <label for="Myster">Mystery</label>
+          </li>
+          <li>
+            <input name="Romance" type="checkbox"/>
+            <label for="Romance">Romance</label>
+          </li>
+          <li>
+            <input name="Science Fiction" type="checkbox"/>
+            <label for="Science Fiction">Science Fiction</label>
+          </li>
+          <li>
+            <input name="TV Movie" type="checkbox"/>
+            <label for="TV Movie">TV Movie</label>
+          </li>
+          <li>
+            <input name="Thriller" type="checkbox"/>
+            <label for="Thriller">Thriller</label>
+          </li>
+          <li>
+            <input name="War" type="checkbox"/>
+            <label for="War">War</label>
+          </li>
+          <li>
+            <input name="Western" type="checkbox"/>
+            <label for="Western">Western</label>
           </li>
         </ul>
+        <!--<li v-for=""> -->
+
         <button type="submit">Find Random Movie!</button>
       </form>
     </div>
@@ -37,6 +96,17 @@
 <script>
 export default {
 
+  data() {
+    return {
+      genres: [],
+      selected_genre_ids: [] 
+      //bind selected genres to the checkboxes
+    }
+  },
+  created() {
+    // call the genres service? or maybe its movie service? to find the available genres
+    // and then loop through them to give options
+  }
 }
 </script>
 
