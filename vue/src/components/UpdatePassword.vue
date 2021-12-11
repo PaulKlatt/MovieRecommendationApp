43d653c788a52a96b7072d1a8efd5a1cc4991fee
Jacqueline <jdkerg@gmail.com>
2021-12-11 11:11:18 -0500

    change password changes

---
+++ vue/src/components/UpdatePassword.vue
@@ -0,0 +1,33 @@
+<template>
+    <div class='updatePassword'>
+    <h3>Reset your password:</h3>
+    <label for="currentPassword">Current Password</label>
+    <form class='updatePassword'>
+    <input
+                     type="password"
+                     id="currentPassword"
+                     class="form-control"
+                     placeholder="Current Password"
+    />
+                    <label for="password">New Password</label>
+
+                    <input
+                      type="password"
+                      id="password"
+                      class="form-control"
+                      placeholder="Password"
+    />
+    <label for="password">Confirm Password</label>
+                      <input
+                      type="password"
+                      id="confirmPassword"
+                      class="form-control"
+                      placeholder="Confirm Password"
+                      required
+                    />
+                     <button class="btn btn-lg btn-primary btn-block" type="submit">
+                      Update Password
+                      </button>
+    </form>
+    </div>
+</template>
