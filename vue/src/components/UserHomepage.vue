b31e8576a940e03701cb6c09c24e6e4ded52534a
Paul Klatt <bcingul8r@gmail.com>
2021-12-16 00:30:19 -0500

    swipeable movie cards

--- vue/src/components/UserHomepage.vue
+++ vue/src/components/UserHomepage.vue
@@ -1,45 +1,69 @@
 <template>
     <div id="user-homepage">
       <!-- this might be its own component, ChooseGenres -->
-      <h2>choose the genres you would like to browse</h2>
-      <form class="genre-form" v-on:submit.prevent='GetRandomMovie()'>
-        <ul class="genres">
-          <li v-for="genre in genres" v-bind:key='genre.id'>
-            <input v-bind:name='genre.name' type='checkbox' v-bind:value='genre.id' v-model='selected_genre_ids' />
-            <label v-bind:for='genre.name'>{{ genre.name }}</label>
-          </li>
-        </ul>
-        <button id='findMoviesRandom' type="submit">find random movie!</button>
-      </form>
-      <button id='swipeUp' v-on:click='SaveToExcluded("Favorite")'>swipe up (add to favorites)</button>
-      <button id='swipeRight' v-on:click='SaveToExcluded("Passed")'>swipe right (pass)</button>
-      <button id='swipeLeft' v-on:click='SaveToExcluded("Uninterested")'>swipe left (completely uninterested)</button>
-      <div id="movie-details" v-if="suggestedMovie">
-        <ul>
-          <li v-if="suggestedMovie.posterPath" id="movieTitle"> {{ suggestedMovie.title }}</li>
-          <li><img v-bind:src="'https://image.tmdb.org/t/p/original' + suggestedMovie.posterPath" /></li>
-           <li v-if="!suggestedMovie.posterPath"><p>Sorry, no poster found for this movie.</p><img id="posterNotFound" src="https://previews.123rf.com/images/lineartestpilot/lineartestpilot1802/lineartestpilot180205606/94855861-cartoon-cat-shrugging-shoulders.jpg?fj=1" /></li>
-          <li id="releaseDate">Release Date: {{ suggestedMovie.releaseDate }}</li>
-          <li id="movieOverview"> {{ suggestedMovie.overview }}</li>
-          <li>Genres: {{ GenreNames(suggestedMovie.genreIds) }}</li>
-        </ul>
+      <div id="topStuff">
+        <h2>choose the genres you would like to browse</h2>
+        <form class="genre-form" v-on:submit.prevent='GetRandomMovie()'>
+          <ul class="genres">
+            <li v-for="genre in genres" v-bind:key='genre.id'>
+              <input v-bind:name='genre.name' type='checkbox' v-bind:value='genre.id' v-model='selected_genre_ids' />
+              <label v-bind:for='genre.name'>{{ genre.name }}</label>
+            </li>
+          </ul>
+          <button id='findMoviesRandom' v-if="!suggestedMovie" type="submit">find a movie!</button>
+        </form>
       </div>
-    </div>
+      <div id='swipeUp' v-if="suggestedMovie" v-on:click='SaveToExcluded("Favorite")' href="#" class="arrow arrow-top">favorite</div>
+      <div id='swipeRight' v-if="suggestedMovie" v-on:click='SaveToExcluded("Passed")' href="#" class="arrow arrow-right">maybe later</div>
+      <div id='swipeLeft' v-if="suggestedMovie" v-on:click='SaveToExcluded("Uninterested")' href="#" class="arrow arrow-left">uninterested</div>
+      <section class="container">
+      <Vue2InteractDraggable
+        :interact-out-of-sight-x-coordinate="1000"
+        :interact-out-of-sight-y-coordinate="1000"
+        :interact-max-rotation="15"
+        :interact-x-threshold="200"
+        :interact-y-threshold="200"
+        @draggedRight='draggedRight'
+        @draggedLeft='draggedLeft'
+        @draggedUp='draggedUp'
+        v-if='isVisible'
+        class="card"
+        id="card">
+        <div class="card__main">
+          <div id="movie-details" v-if="suggestedMovie">
+            <ul>
+              <li id="movieTitle"> {{ suggestedMovie.title }}</li>
+              <li>Movie Id: {{ suggestedMovie.id }}
+              <li v-if="suggestedMovie.posterPath"><img v-bind:src="'https://image.tmdb.org/t/p/original' + suggestedMovie.posterPath" /></li>
+              <li v-if="!suggestedMovie.posterPath"><p>Sorry, no poster found for this movie.</p><img id="posterNotFound" src="https://previews.123rf.com/images/lineartestpilot/lineartestpilot1802/lineartestpilot180205606/94855861-cartoon-cat-shrugging-shoulders.jpg?fj=1" /></li>
+              <li id="releaseDate">Release Date: {{ suggestedMovie.releaseDate }}</li>
+              <li id="movieOverview"> {{ suggestedMovie.overview }}</li>
+              <li>Genres: {{ GenreNames(suggestedMovie.genreIds) }}</li>
+            </ul>
+          </div>    
+        </div>
+      </Vue2InteractDraggable>
+  </section>
+  </div>
 </template>
 
 <script>
 import movieService from '../services/MovieService';
 import userService from '../services/UserService';
+import { Vue2InteractDraggable } from 'vue2-interact'
 
 export default {
+
+  components: { Vue2InteractDraggable },
 
   data() {
     return {
       genres: [],
       selected_genre_ids: [],
       suggestedMovie: false,
-      currentUser: false
-      //bind selected genres to the checkboxes
+      currentUser: false,
+      isVisible: true
+      //test: document.getElementById("card").cloneNode(true)
     }
   },
   created() {
@@ -63,6 +87,11 @@ export default {
     GetRandomMovie(){
       movieService.getRandomMovie(this.queryString + "/users/" + this.$store.state.user.userId).then( response => {
         this.suggestedMovie = response.data;
+        setTimeout(() => this.isVisible = false, 200)
+        setTimeout(() => {
+
+        this.isVisible = true
+      }, 200);
     }).catch ( error => {
       //ERROR HANDLING
       console.log('Something messed up 2: ' + error)
@@ -86,22 +115,10 @@ export default {
           }
         }
         userService.saveToExcluded(this.$store.state.user.userId, movieInfo);
-
         this.GetRandomMovie();
       },
-      swipeHandler(direction){ 
-        if(direction == 'right'){
-          this.SaveToExcluded("Passed");
-        } else if (direction == 'top'){
-          this.SaveToExcluded("Favorite");
-        } else if (direction == 'left'){
-          this.SaveToExcluded("Uninterested");
-        }
-        console.log(direction);
-      
-    },
 
-      GenreNames(genreArray) {
+         GenreNames(genreArray) {
         const allGenreList = this.$store.state.genres
         let containedGenreNames = '';
         genreArray.forEach( cId => {
@@ -117,7 +134,23 @@ export default {
           })
         })
         return containedGenreNames;
-      }
+      },
+
+      draggedLeft() {
+        this.SaveToExcluded("Uninterested")
+        
+      },
+
+      draggedRight() {
+        this.SaveToExcluded("Passed")
+
+      },
+
+      draggedUp() {
+        this.SaveToExcluded("Favorite")
+
+      },
+
   }
 }
  
@@ -128,16 +161,29 @@ export default {
 <style>
 ul {
   list-style-type: none;
+  margin: auto;
+}
+
+#movie-details li {
+  width:90%;
+}
+
+img {
+  display: block;
+  margin: 0 auto;
+  width: 80%;
+  height: 70%;
 }
 
 #findMoviesRandom, #swipeUp, #swipeRight, #swipeLeft {
   background-color: #f67280; font-size: larger; 
-      color: #355c7d; border: 1px solid #266DB6; box-sizing: border-box; font-weight: 700;
-      line-height: 24px; padding: 16px 23px; position: relative; text-decoration: none;  
-      box-shadow: 3px 3px #f8b195;
-      padding: 0.25em 0.5em;
-      user-select: none; touch-action: manipulation;
-      cursor: pointer;
+  color: #355c7d; border: 1px solid #266DB6; box-sizing: border-box; font-weight: 700;
+  line-height: 24px; padding: 16px 23px; position: relative; text-decoration: none;
+  box-shadow: 3px 3px #f8b195;
+  padding: 0.25em 0.5em;
+  user-select: none; touch-action: manipulation;
+  cursor: pointer;
+  width:300px;
 }
 
 #findMoviesRandom:active {
@@ -164,6 +210,80 @@ ul {
   left: 5px;
 }
 
+.arrow{
+  position: relative;
+  height:40px;
+  line-height: 40px;
+  margin:auto; 
+  text-align:center;
 
+}
+
+.arrow-top:before{
+  position: absolute;
+  top: -10px;
+  left:50%;
+  margin-left: -10px;
+  content:"";
+  border-left: 10px solid #355c7d;
+  border-right: 10px solid #355c7d;
+  border-bottom: 10px solid #f67280; 
+}
+
+.arrow-right:after{
+    content: "";
+    position: absolute;
+    right: -20px;
+    top: 0;
+    border-top: 20px solid#355c7d;
+    border-bottom: 20px solid #355c7d;
+    border-left: 20px solid #f67280; 
+}
+
+.arrow-left:before{
+    content: "";
+    position: absolute;
+    left: -20px;
+    top: 0;
+    border-top: 20px solid #355c7d;
+    border-bottom: 20px solid#355c7d;
+    border-right: 20px solid #f67280; 
+}
+
+#movie-details {
+  background-color: #f67280;
+  width: 100%;
+  margin: 20px auto;
+  border-radius: 10%;
+}
+
+#topStuff {
+  grid-area: topStuff;
+}
+
+#swipeUp {
+  grid-area: swipeUp;
+}
+
+#swipeLeft {
+  grid-area: swipeLeft;
+}
+
+#swipeRight {
+  grid-area: swipeRight;
+}
+
+.container {
+  grid-area: container;
+}
+
+#user-homepage {
+  display: grid;
+  grid-template-columns: 1fr 1fr 1fr;
+  grid-template-areas:". topStuff ."
+                     ". swipeUp ."
+                     "swipeLeft container swipeRight";
+                     
+}
 
 </style>
--- vue/src/views/Account.vue
+++ vue/src/views/Account.vue
@@ -2,7 +2,7 @@
   <div>
       <account-details v-if="!showForm"/>
       <update-account v-if="showForm"/>
-      <button v-if="!showForm" v-on:click="updateAccount()">Update Account Details</button>
+      <button id="updateAccount" v-if="!showForm" v-on:click="updateAccount()">Update Account Details</button>
   </div>
 </template>
 
@@ -26,5 +26,19 @@ methods: {
 </script>
 
 <style>
+#updateAccount {
+  background-color: #f67280; font-size: larger; 
+      color: #355c7d; border: 1px solid #266DB6; box-sizing: border-box; font-weight: 700;
+      line-height: 24px; padding: 16px 23px; position: relative; text-decoration: none;  
+      box-shadow: 3px 3px #f8b195;
+      padding: 0.25em 0.5em;
+      user-select: none; touch-action: manipulation;
+      cursor: pointer;
+}
 
+#updateAccount:active {
+  box-shadow: 0px 0px 0px 0px;
+  top: 5px;
+  left: 5px;
+}
 </style>
