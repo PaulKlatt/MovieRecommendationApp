81924079cb63af71745988ba122409e5552027c0
Paul Klatt <bcingul8r@gmail.com>
2021-12-16 16:49:12 -0500

    presentation data

--- dotnet/database/capstone.sql
+++ dotnet/database/capstone.sql
@@ -84,24 +84,48 @@ INSERT INTO users (username, password_hash, salt, user_role) VALUES ('user','Jg4
 INSERT INTO users (username, password_hash, salt, user_role) VALUES ('admin','YhyGVQ+Ch69n4JMBncM4lNF/i9s=', 'Ar/aB2thQTI=','admin');
 
 --populate presentation data
-INSERT INTO movieCards (movie_id, title, poster_path, genre_ids) VALUES (96984, 'Pleasure Shop on the Avenue', '/lpwCNexA5N6gUZeXlR8bXv8ZCI1.jpg', '53')
+INSERT INTO movieCards (movie_id, title, poster_path, genre_ids) VALUES (96984, 'All Dogs Go To Heaven', '/lpwCNexA5N6gUZeXlR8bXv8ZCI1.jpg', '53')
 INSERT INTO users_excludedMovies (movie_id, user_id, opinion, removal_tracker) VALUES (96984, 2, 'Banned', 0)
 
-INSERT INTO movieCards (movie_id, title, poster_path, genre_ids) VALUES (22705, 'Cheeky', '/7gqcEgJfTltbLo76nZTl8umgM1Y.jpg', '35|18|10749')
+INSERT INTO movieCards (movie_id, title, poster_path, genre_ids) VALUES (22705, 'Air Bud', '/7gqcEgJfTltbLo76nZTl8umgM1Y.jpg', '35|18|10749')
 INSERT INTO users_excludedMovies (movie_id, user_id, opinion, removal_tracker) VALUES (22705, 2, 'Banned', 0)
 
-INSERT INTO movieCards (movie_id, title, poster_path, genre_ids) VALUES (463998, 'Paradise of Innocence', '/zavz3iYAOgBpE05UqamLmah0Tlj.jpg', '16')
+INSERT INTO movieCards (movie_id, title, poster_path, genre_ids) VALUES (463998, 'The Inappropriate Movie Example', '/zavz3iYAOgBpE05UqamLmah0Tlj.jpg', '16')
 INSERT INTO users_excludedMovies (movie_id, user_id, opinion, removal_tracker) VALUES (463998, 2, 'Banned', 0)
 
-INSERT INTO movieCards (movie_id, title, poster_path, genre_ids) VALUES (26728, 'Garfield''s Fun Fest', '/zavz3iYAOgBpE05UqamLmah0Tlj.jpg', '35|16|10751')
-INSERT INTO users_excludedMovies (movie_id, user_id, opinion, removal_tracker) VALUES (26728, 1, 'Favorite', 0)
+INSERT INTO movieCards (movie_id, title, poster_path, genre_ids) VALUES (464052, 'Beethoven', '/8UlWHLMpgZm9bx6QYh0NFoq67TZ.jpg', '28|12|14')
+INSERT INTO users_excludedMovies (movie_id, user_id, opinion, removal_tracker) VALUES (464052, 2, 'Banned', 0)
 
-INSERT INTO movieCards (movie_id, title, poster_path, genre_ids) VALUES (625404, 'Mother and Daughter: Full of Breast Milk', '/b8mYDyDYKuFrPxxrvqJXw3W0aj0.jpg', '16')
-INSERT INTO users_excludedMovies (movie_id, user_id, opinion, removal_tracker) VALUES (625404, 2, 'Banned', 0)
+INSERT INTO movieCards (movie_id, title, poster_path, genre_ids) VALUES (582186, 'Old Yeller', '/8UlWHLMpgZm9bx6QYh0NFoq67TZ.jpg', '10751')
+INSERT INTO users_excludedMovies (movie_id, user_id, opinion, removal_tracker) VALUES (582186, 2, 'Banned', 0)
+
+INSERT INTO movieCards (movie_id, title, poster_path, genre_ids) VALUES (899338, 'Clifford', '/8UlWHLMpgZm9bx6QYh0NFoq67TZ.jpg', '10751')
+INSERT INTO users_excludedMovies (movie_id, user_id, opinion, removal_tracker) VALUES (899338, 2, 'Banned', 0)
+
+INSERT INTO movieCards (movie_id, title, poster_path, genre_ids) VALUES (664413, '101 Dalmations', '/8UlWHLMpgZm9bx6QYh0NFoq67TZ.jpg', '10751')
+INSERT INTO users_excludedMovies (movie_id, user_id, opinion, removal_tracker) VALUES (664413, 2, 'Banned', 0)
+
+INSERT INTO movieCards (movie_id, title, poster_path, genre_ids) VALUES (50701, 'Marley & Me', '/8UlWHLMpgZm9bx6QYh0NFoq67TZ.jpg', '10751')
+INSERT INTO users_excludedMovies (movie_id, user_id, opinion, removal_tracker) VALUES (59507, 2, 'Banned', 0)
+
+INSERT INTO movieCards (movie_id, title, poster_path, genre_ids) VALUES (67308, 'Balto', '/8UlWHLMpgZm9bx6QYh0NFoq67TZ.jpg', '28|14')
+INSERT INTO users_excludedMovies (movie_id, user_id, opinion, removal_tracker) VALUES (67308, 2, 'Banned', 0)
+
+INSERT INTO movieCards (movie_id, title, poster_path, genre_ids) VALUES (403310, 'Snow Dogs', '/8UlWHLMpgZm9bx6QYh0NFoq67TZ.jpg', '28|14')
+INSERT INTO users_excludedMovies (movie_id, user_id, opinion, removal_tracker) VALUES (403310, 2, 'Banned', 0)
+
+INSERT INTO movieCards (movie_id, title, poster_path, genre_ids) VALUES (26728, 'Garfield''s Fun Fest', '/vsCHc3vb5xdS9nJ5YdFSY8p2ujO.jpg', '35|16|10751')
+INSERT INTO users_excludedMovies (movie_id, user_id, opinion, removal_tracker) VALUES (26728, 1, 'Favorite', 0)
 
 INSERT INTO movieCards (movie_id, title, poster_path, genre_ids) VALUES (56202, 'Pettson and Findus', '/8mGYnUkthgdvs0ol6gyjqpNYCoe.jpg', '35|16|10751')
 INSERT INTO users_excludedMovies (movie_id, user_id, opinion, removal_tracker) VALUES (56202, 1, 'Favorite', 0)
 
 INSERT INTO movieCards (movie_id, title, poster_path, genre_ids) VALUES (16390, 'Scooby-Doo! and the Samurai Sword', '/qJ0UlWWrBn2fnAg4SifTJbDUuaf.jpg', '10751|9648|16|35|28|14')
 INSERT INTO users_excludedMovies (movie_id, user_id, opinion, removal_tracker) VALUES (16390, 1, 'Favorite', 0)
+
+INSERT INTO movieCards (movie_id, title, poster_path, genre_ids) VALUES (587807, 'Tom & Jerry', '/8XZI9QZ7Pm3fVkigWJPbrXCMzjq.jpg', '10751|16|35')
+INSERT INTO users_excludedMovies (movie_id, user_id, opinion, removal_tracker) VALUES (587807, 1, 'Favorite', 0)
+
+INSERT INTO movieCards (movie_id, title, poster_path, genre_ids) VALUES (53002, 'Tom & Jerry', '/lNU2o8eMy0FjSpALCxtxf9lEZ1Q.jpg', '10751|16|35')
+INSERT INTO users_excludedMovies (movie_id, user_id, opinion, removal_tracker) VALUES (53002, 1, 'Favorite', 0)
 GO
